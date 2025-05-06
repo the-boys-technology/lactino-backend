1ec5dcf4a3dff55f4e419fb07bbde7a59fe5f848
Guiioff <guilhermerivaldy288@hotmail.com>
2025-05-06 19:38:17 -0300

    feat: cria estrutura inicial do projeto

---
+++ src/main/java/br/com/tbt/lactino/LactinoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.tbt.lactino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LactinoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LactinoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/tbt/lactino/LactinoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tbt.lactino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LactinoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
