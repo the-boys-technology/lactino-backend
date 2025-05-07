4b035c80635204a5f007bad19eef68296b817e17
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-05-07 19:20:25 -0300

    feat: configura as anotações JPA para a entidade MovimentacaoEstoque e seus atributos

--- src/main/java/br/com/tbt/lactino/model/MovimentacaoEstoque.java
+++ src/main/java/br/com/tbt/lactino/model/MovimentacaoEstoque.java
@@ -1,15 +1,31 @@
 package br.com.tbt.lactino.model;
 
 import br.com.tbt.lactino.model.enums.TipoMovimentacaoEstoque;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity
+@Table(name = "movimentacoes_estoque")
 public class MovimentacaoEstoque {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Column(nullable = false)
   private LocalDateTime dataMovimentacao;
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
   private TipoMovimentacaoEstoque tipo;
+
+  @Column(nullable = false)
   private Double quantidade;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "insumo_id", nullable = false)
   private Insumo insumo;
-  private String descricao;
+
+  private String descricao; // todo Validar se a descrição é opcional ou não
 }
