136e09147b83cd39b89ecc72fbfbbe0455616b0c
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-05-07 18:59:25 -0300

    feat: configura as anotações JPA para a entidade Insumo e seus atributos

--- src/main/java/br/com/tbt/lactino/model/Insumo.java
+++ src/main/java/br/com/tbt/lactino/model/Insumo.java
@@ -2,20 +2,46 @@ package br.com.tbt.lactino.model;
 
 import br.com.tbt.lactino.model.enums.CategoriaInsumoEnum;
 import br.com.tbt.lactino.model.enums.StatusInsumoEnum;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Entity
+@Table(name = "insumos")
 public class Insumo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false) //todo Validar se o nome deve ser único na base
     private String nome;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private CategoriaInsumoEnum categoria;
+
+    @Column(nullable = false)
     private String unidadeMedida;
+
+    @Column(nullable = false)
     private Double quantidadeTotal;
+
+    @Column(nullable = false)
     private Double quantidadeMinima;
+
+    @Column(nullable = false)
     private LocalDate validade;
+
+    @Column(nullable = false)
     private BigDecimal preco;
+
+    @Column(nullable = false)
     private String fornecedor;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private StatusInsumoEnum status;
 }
