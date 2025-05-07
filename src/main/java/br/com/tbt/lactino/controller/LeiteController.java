a7966f56ceeaa72e09c8232ddb7b5a23c902c779
Guiioff <guilhermerivaldy288@hotmail.com>
2025-05-07 17:38:22 -0300

    feat: define método para salvar um leite no controller

--- src/main/java/br/com/tbt/lactino/controller/LeiteController.java
+++ src/main/java/br/com/tbt/lactino/controller/LeiteController.java
@@ -1,4 +1,24 @@
 package br.com.tbt.lactino.controller;
 
+import br.com.tbt.lactino.controller.request.LeiteDTO;
+import br.com.tbt.lactino.service.LeiteService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/leites")
 public class LeiteController {
+
+    private final LeiteService leiteService;
+
+    public LeiteController(LeiteService leiteService) {
+        this.leiteService = leiteService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void salvarLeite(@RequestBody @Valid LeiteDTO leiteDTO) {
+        leiteService.salvarLeite(leiteDTO);
+    }
 }
