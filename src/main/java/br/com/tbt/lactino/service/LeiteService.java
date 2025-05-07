d253b9acf0ba24ee440f40492ba9f7fee94225a8
Guiioff <guilhermerivaldy288@hotmail.com>
2025-05-07 18:41:45 -0300

    feat: define método para buscar leite pelo id

--- src/main/java/br/com/tbt/lactino/service/LeiteService.java
+++ src/main/java/br/com/tbt/lactino/service/LeiteService.java
@@ -1,7 +1,12 @@
 package br.com.tbt.lactino.service;
 
 import br.com.tbt.lactino.controller.request.LeiteDTO;
+import br.com.tbt.lactino.controller.response.LeiteDetalhadoResponse;
+
+import java.util.UUID;
 
 public interface LeiteService {
     void salvarLeite(LeiteDTO leiteDTO);
+
+    LeiteDetalhadoResponse buscarLeite(UUID leiteId);
 }
