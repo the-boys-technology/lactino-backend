27a15dc4050876cc90d5e99adb5f64bae9222bd6
Jonas Santos <jonashenrique965@gmail.com>
2025-05-08 00:01:39 -0300

    feat: modelos de Transacao e ItemTransacao

---
+++ src/main/java/br/com/tbt/lactino/model/Transacao.java
@@ -0,0 +1,48 @@
+package br.com.tbt.lactino.model;
+
+import br.com.tbt.lactino.model.enums.FormaPagamento;
+import br.com.tbt.lactino.model.enums.TipoTransacao;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transacoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Transacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoTransacao tipo;
+
+    @Column(nullable = false)
+    private LocalDateTime data;
+
+    @Column(name = "valor_total", nullable = false)
+    private BigDecimal valorTotal;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "forma_pagamento", nullable = false)
+    private FormaPagamento formaPagamento;
+
+    private Long clienteId;
+
+    private Long fornecedorId;
+
+    private Long leiteId;
+
+    private Long laticinioId;
+
+    private String descricao;
+}
---
+++ src/main/java/br/com/tbt/lactino/model/enums/CategoriaProduto.java
@@ -0,0 +1,10 @@
+package br.com.tbt.lactino.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum CategoriaProduto {
+    LEITE,
+    LATICINIO,
+    INSUMO
+}
