d5f73a9d31410953499cba55963c9db21bcd35b4
Guiioff <guilhermerivaldy288@hotmail.com>
2025-05-07 22:40:55 -0300

    feat: criação da specfication de laticinio

--- src/main/java/br/com/tbt/lactino/repository/LaticinioRepository.java
+++ src/main/java/br/com/tbt/lactino/repository/LaticinioRepository.java
@@ -1,12 +1,14 @@
 package br.com.tbt.lactino.repository;
 
 import br.com.tbt.lactino.model.Laticinio;
+import br.com.tbt.lactino.model.Leite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface LaticinioRepository extends JpaRepository<Laticinio, UUID> {
+public interface LaticinioRepository extends JpaRepository<Laticinio, UUID>, JpaSpecificationExecutor<Laticinio> {
 
     Optional<Laticinio> findById(UUID id);
 }
