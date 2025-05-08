57a7752dd65dfb42518245940e59e66d21ebf5a0
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-05-07 21:14:52 -0300

    feat: implementa método para consulta de insumo

--- src/main/java/br/com/tbt/lactino/service/impl/InsumoServiceImpl.java
+++ src/main/java/br/com/tbt/lactino/service/impl/InsumoServiceImpl.java
@@ -2,6 +2,7 @@ package br.com.tbt.lactino.service.impl;
 
 import br.com.tbt.lactino.controller.request.CadastrarInsumoDTO;
 import br.com.tbt.lactino.controller.response.CadastrarInsumoResponse;
+import br.com.tbt.lactino.controller.response.InsumoResponse;
 import br.com.tbt.lactino.model.Insumo;
 import br.com.tbt.lactino.model.enums.CategoriaInsumoEnum;
 import br.com.tbt.lactino.model.enums.StatusInsumoEnum;
@@ -10,6 +11,8 @@ import br.com.tbt.lactino.service.InsumoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -36,5 +39,28 @@ public class InsumoServiceImpl implements InsumoService {
 
     Insumo insumoSalvo = this.insumoRepository.save(novoInsumo);
     return new CadastrarInsumoResponse(insumoSalvo.getId(), insumoSalvo.getNome());
+  }
+
+  @Override
+  public InsumoResponse buscarPorId(String id) {
+    UUID idInsumo;
+
+    try {
+      idInsumo = UUID.fromString(id);
+    } catch (IllegalArgumentException e) {
+      String erro = String.format("O ID %s é inválido", id);
+      throw new RuntimeException(erro);
+    }
+
+    Insumo insumo =
+        this.insumoRepository
+            .findById(idInsumo)
+            .orElseThrow(
+                () -> {
+                  String erro = String.format("Não foi encontrado nenhum insumo com o ID %s", id);
+                  return new RuntimeException(erro);
+                });
+
+    return new InsumoResponse(insumo);
   }
 }
