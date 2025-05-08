0090c1176f92641b766bf5b775a216a42e062417
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-05-07 21:14:39 -0300

    feat: declarando método para consulta de insumo

--- src/main/java/br/com/tbt/lactino/service/InsumoService.java
+++ src/main/java/br/com/tbt/lactino/service/InsumoService.java
@@ -2,7 +2,10 @@ package br.com.tbt.lactino.service;
 
 import br.com.tbt.lactino.controller.request.CadastrarInsumoDTO;
 import br.com.tbt.lactino.controller.response.CadastrarInsumoResponse;
+import br.com.tbt.lactino.controller.response.InsumoResponse;
 
 public interface InsumoService {
   CadastrarInsumoResponse cadastrarInsumo(CadastrarInsumoDTO dto);
+
+  InsumoResponse buscarPorId(String id);
 }
