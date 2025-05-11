88ef51b91e9a7173c185c533448f1226edfce008
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-05-11 14:09:33 -0300

    feat: implementa endpoint de consulta de movimentação

--- src/main/java/br/com/tbt/lactino/controller/MovimentacaoEstoqueController.java
+++ src/main/java/br/com/tbt/lactino/controller/MovimentacaoEstoqueController.java
@@ -1,22 +1,27 @@
 package br.com.tbt.lactino.controller;
 
 import br.com.tbt.lactino.controller.request.RegistrarMovimentacaoDTO;
+import br.com.tbt.lactino.controller.response.MovimentacaoEstoqueResponse;
 import br.com.tbt.lactino.controller.response.RegistrarMovimentacaoResponse;
 import br.com.tbt.lactino.service.MovimentacaoEstoqueService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/movimentacoes")
 @RequiredArgsConstructor
 public class MovimentacaoEstoqueController {
   private final MovimentacaoEstoqueService movimentacaoEstoqueService;
+
+  @GetMapping("/{id}")
+  public ResponseEntity<MovimentacaoEstoqueResponse> detalharMovimentacao(
+      @PathVariable(value = "id") String id) {
+    MovimentacaoEstoqueResponse response = this.movimentacaoEstoqueService.buscarMovimentacao(id);
+    return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
 
   @PostMapping
   public ResponseEntity<RegistrarMovimentacaoResponse> registrarMovimentacao(
