c540de06756b131070af1f7614af15a508d782e6
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-05-11 14:08:55 -0300

    feat: adiciona método de consulta à interface de serviço

--- src/main/java/br/com/tbt/lactino/service/MovimentacaoEstoqueService.java
+++ src/main/java/br/com/tbt/lactino/service/MovimentacaoEstoqueService.java
@@ -1,9 +1,12 @@
 package br.com.tbt.lactino.service;
 
 import br.com.tbt.lactino.controller.request.RegistrarMovimentacaoDTO;
+import br.com.tbt.lactino.controller.response.MovimentacaoEstoqueResponse;
 import br.com.tbt.lactino.controller.response.RegistrarMovimentacaoResponse;
 
 public interface MovimentacaoEstoqueService {
 
   RegistrarMovimentacaoResponse registrarMovimentacao(RegistrarMovimentacaoDTO dto);
+
+  MovimentacaoEstoqueResponse buscarMovimentacao(String id);
 }
