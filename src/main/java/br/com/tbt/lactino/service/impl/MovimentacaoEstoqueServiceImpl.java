a03faff7422241848e148f03bb9b1715761c051c
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-05-11 13:57:01 -0300

    feat: cria classe de serviço para movimentações de estoque

---
+++ src/main/java/br/com/tbt/lactino/service/impl/MovimentacaoEstoqueServiceImpl.java
@@ -0,0 +1,58 @@
+package br.com.tbt.lactino.service.impl;
+
+import br.com.tbt.lactino.controller.request.RegistrarMovimentacaoDTO;
+import br.com.tbt.lactino.controller.response.RegistrarMovimentacaoResponse;
+import br.com.tbt.lactino.model.MovimentacaoEstoque;
+import br.com.tbt.lactino.model.enums.TipoMovimentacaoEstoque;
+import br.com.tbt.lactino.repository.MovimentacaoEstoqueRepository;
+import br.com.tbt.lactino.service.InsumoService;
+import br.com.tbt.lactino.service.MovimentacaoEstoqueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class MovimentacaoEstoqueServiceImpl implements MovimentacaoEstoqueService {
+  private final InsumoService insumoService;
+  private final MovimentacaoEstoqueRepository movimentacaoEstoqueRepository;
+
+  @Override
+  @Transactional
+  public RegistrarMovimentacaoResponse registrarMovimentacao(RegistrarMovimentacaoDTO dto) {
+    TipoMovimentacaoEstoque tipo;
+    UUID idInsumo;
+
+    try {
+      tipo = TipoMovimentacaoEstoque.valueOf(dto.tipo());
+    } catch (IllegalArgumentException ex) {
+      String erro = String.format("O tipo de movimentação %s é inválido", ex);
+      throw new RuntimeException(erro);
+    }
+
+    try {
+      idInsumo = UUID.fromString(dto.idInsumo());
+    } catch (IllegalArgumentException ex) {
+      String erro = String.format("O ID de insumo %s é inválido", ex);
+      throw new RuntimeException(erro);
+    }
+
+    MovimentacaoEstoque novaMovimentacaoEstoque = dto.toEntity();
+    novaMovimentacaoEstoque.setTipo(tipo);
+    novaMovimentacaoEstoque.setDataMovimentacao(LocalDateTime.now());
+    novaMovimentacaoEstoque.setInsumo(this.insumoService.buscarInsumo(idInsumo));
+
+    MovimentacaoEstoque movimentacaoEstoqueSalva =
+        this.movimentacaoEstoqueRepository.save(novaMovimentacaoEstoque);
+    this.insumoService.alterarQuantidade(idInsumo, dto.quantidade(), tipo);
+
+    return RegistrarMovimentacaoResponse.builder()
+        .id(movimentacaoEstoqueSalva.getId())
+        .data(movimentacaoEstoqueSalva.getDataMovimentacao())
+        .tipo(movimentacaoEstoqueSalva.getTipo().getDescricao())
+        .build();
+  }
+}
