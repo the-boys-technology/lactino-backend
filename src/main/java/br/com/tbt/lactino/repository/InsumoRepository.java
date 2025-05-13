2dbf6dd422cabcc267f40b414c54511a145b68e7
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-05-13 08:18:18 -0300

    feat: configura a interface de repositório para lidar com specifications

--- src/main/java/br/com/tbt/lactino/repository/InsumoRepository.java
+++ src/main/java/br/com/tbt/lactino/repository/InsumoRepository.java
@@ -2,7 +2,9 @@ package br.com.tbt.lactino.repository;
 
 import br.com.tbt.lactino.model.Insumo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface InsumoRepository extends JpaRepository<Insumo, UUID> {}
+public interface InsumoRepository
+    extends JpaRepository<Insumo, UUID>, JpaSpecificationExecutor<Insumo> {}
