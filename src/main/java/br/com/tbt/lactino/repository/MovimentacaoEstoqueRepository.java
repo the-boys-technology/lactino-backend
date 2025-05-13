7491957d7215f33cd0b597eca280247ab8e7085b
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-05-13 08:36:58 -0300

    feat: modifica o repositório de movimentações para suportar specifications

--- src/main/java/br/com/tbt/lactino/repository/MovimentacaoEstoqueRepository.java
+++ src/main/java/br/com/tbt/lactino/repository/MovimentacaoEstoqueRepository.java
@@ -2,7 +2,10 @@ package br.com.tbt.lactino.repository;
 
 import br.com.tbt.lactino.model.MovimentacaoEstoque;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface MovimentacaoEstoqueRepository extends JpaRepository<MovimentacaoEstoque, UUID> {}
+public interface MovimentacaoEstoqueRepository
+    extends JpaRepository<MovimentacaoEstoque, UUID>,
+        JpaSpecificationExecutor<MovimentacaoEstoque> {}
