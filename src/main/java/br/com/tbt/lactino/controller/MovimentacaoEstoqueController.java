7de17ec811877f36c755eecd785b20a4ad565f93
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-05-13 08:37:49 -0300

    feat: implementa o endpoint de listagem de movimentações

--- src/main/java/br/com/tbt/lactino/controller/MovimentacaoEstoqueController.java
+++ src/main/java/br/com/tbt/lactino/controller/MovimentacaoEstoqueController.java
@@ -1,20 +1,52 @@
 package br.com.tbt.lactino.controller;
 
+import br.com.tbt.lactino.controller.request.MovimentacaoFiltro;
 import br.com.tbt.lactino.controller.request.RegistrarMovimentacaoDTO;
 import br.com.tbt.lactino.controller.response.MovimentacaoEstoqueResponse;
+import br.com.tbt.lactino.controller.response.PaginaDTO;
 import br.com.tbt.lactino.controller.response.RegistrarMovimentacaoResponse;
+import br.com.tbt.lactino.model.enums.TipoMovimentacaoEstoque;
 import br.com.tbt.lactino.service.MovimentacaoEstoqueService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/movimentacoes")
 @RequiredArgsConstructor
 public class MovimentacaoEstoqueController {
   private final MovimentacaoEstoqueService movimentacaoEstoqueService;
+
+  @GetMapping
+  public ResponseEntity<PaginaDTO<MovimentacaoEstoqueResponse>> listar(
+      @RequestParam(required = false) TipoMovimentacaoEstoque tipo,
+      @RequestParam(required = false, name = "data_inicial")
+          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+          LocalDate dataInicial,
+      @RequestParam(required = false, name = "data_final")
+          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+          LocalDate dataFinal,
+      @RequestParam(required = false, name = "insumo_id") Long insumoId,
+      Pageable pageable) {
+    MovimentacaoFiltro filtro =
+        MovimentacaoFiltro.builder()
+            .tipo(tipo)
+            .dataInicial(dataInicial)
+            .dataFinal(dataFinal)
+            .insumoID(insumoId)
+            .build();
+
+    Page<MovimentacaoEstoqueResponse> resposta =
+        this.movimentacaoEstoqueService.listarMovimentacoes(filtro, pageable);
+    return ResponseEntity.status(HttpStatus.OK).body(new PaginaDTO<>(resposta));
+  }
 
   @GetMapping("/{id}")
   public ResponseEntity<MovimentacaoEstoqueResponse> detalharMovimentacao(
