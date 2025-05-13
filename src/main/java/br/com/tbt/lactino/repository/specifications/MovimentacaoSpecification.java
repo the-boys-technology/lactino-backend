13e5707655e192176b6f2ab7b27a22c1e9bccdf6
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-05-13 08:36:37 -0300

    feat: cria specification para a listagem de movimentacoes

---
+++ src/main/java/br/com/tbt/lactino/repository/specifications/MovimentacaoSpecification.java
@@ -0,0 +1,35 @@
+package br.com.tbt.lactino.repository.specifications;
+
+import br.com.tbt.lactino.controller.request.MovimentacaoFiltro;
+import br.com.tbt.lactino.model.MovimentacaoEstoque;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovimentacaoSpecification {
+  public static Specification<MovimentacaoEstoque> comFiltros(MovimentacaoFiltro filtro) {
+    return (root, query, cb) -> {
+      List<Predicate> predicates = new ArrayList<>();
+
+      if (filtro.tipo() != null) {
+        predicates.add(cb.equal(root.get("tipo"), filtro.tipo()));
+      }
+
+      if (filtro.dataInicial() != null) {
+        predicates.add(cb.greaterThanOrEqualTo(root.get("dataMovimentacao"), filtro.dataInicial()));
+      }
+
+      if (filtro.dataFinal() != null) {
+        predicates.add(cb.lessThanOrEqualTo(root.get("dataMovimentacao"), filtro.dataFinal()));
+      }
+
+      if (filtro.insumoID() != null) {
+        predicates.add(cb.equal(root.get("insumo").get("id"), filtro.insumoID()));
+      }
+
+      return cb.and(predicates.toArray(new Predicate[0]));
+    };
+  }
+}
