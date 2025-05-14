71a3622237f10037e6f91ea3618e53cff8bb65e8
Guiioff <guilhermerivaldy288@hotmail.com>
2025-05-13 21:15:55 -0300

    feat: define método para buscar leites que estão vencendo

--- src/main/java/br/com/tbt/lactino/repository/LeiteRepository.java
+++ src/main/java/br/com/tbt/lactino/repository/LeiteRepository.java
@@ -1,13 +1,18 @@
 package br.com.tbt.lactino.repository;
 
 import br.com.tbt.lactino.model.Leite;
+import br.com.tbt.lactino.model.enums.StatusLeiteEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface LeiteRepository extends JpaRepository<Leite, UUID> , JpaSpecificationExecutor<Leite> {
 
     Optional<Leite> findById(UUID id);
+
+    List<Leite> findByStatusAndDataValidadeLessThanEqual(StatusLeiteEnum status, LocalDate dataLimite);
 }
