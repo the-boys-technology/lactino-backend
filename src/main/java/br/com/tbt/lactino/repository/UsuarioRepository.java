fcc6a651e32379cbb21b9178024ac9211eca052d
Guiioff <guilhermerivaldy288@hotmail.com>
2025-05-19 18:04:34 -0300

    feat: cria repository de Usuario

---
+++ src/main/java/br/com/tbt/lactino/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package br.com.tbt.lactino.repository;
+
+import br.com.tbt.lactino.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    Optional<Usuario> findByEmail(String email);
+}
