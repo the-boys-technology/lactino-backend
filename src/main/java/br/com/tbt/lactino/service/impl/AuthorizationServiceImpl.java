f40eb579fa8123aed6f2e900776fadb1dc9b88bd
Guiioff <guilhermerivaldy288@hotmail.com>
2025-05-19 18:05:10 -0300

    feat: define e implementa service de autorização

---
+++ src/main/java/br/com/tbt/lactino/service/impl/AuthorizationServiceImpl.java
@@ -0,0 +1,23 @@
+package br.com.tbt.lactino.service.impl;
+
+import br.com.tbt.lactino.repository.UsuarioRepository;
+import br.com.tbt.lactino.service.AuthorizationService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationServiceImpl implements AuthorizationService {
+
+    private UsuarioRepository usuarioRepository;
+
+    public AuthorizationServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com e-mail: " + username));
+    }
+}
