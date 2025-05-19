09740916125fe1a958df7729725bf6f0a0f38102
Guiioff <guilhermerivaldy288@hotmail.com>
2025-05-19 19:41:27 -0300

    feat: adiciona resposta de token para o login

--- src/main/java/br/com/tbt/lactino/controller/AuthController.java
+++ src/main/java/br/com/tbt/lactino/controller/AuthController.java
@@ -2,6 +2,9 @@ package br.com.tbt.lactino.controller;
 
 import br.com.tbt.lactino.controller.request.LoginDTO;
 import br.com.tbt.lactino.controller.request.RegistroDTO;
+import br.com.tbt.lactino.controller.response.LoginResponseDTO;
+import br.com.tbt.lactino.model.Usuario;
+import br.com.tbt.lactino.security.TokenService;
 import br.com.tbt.lactino.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -15,17 +18,22 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final UsuarioService usuarioService;
+    private final TokenService tokenService;
 
-    public AuthController(AuthenticationManager authenticationManager, UsuarioService usuarioService) {
+    public AuthController(AuthenticationManager authenticationManager, UsuarioService usuarioService, TokenService tokenService) {
         this.authenticationManager = authenticationManager;
         this.usuarioService = usuarioService;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    public void login(@RequestBody @Valid LoginDTO dto){
+    public LoginResponseDTO login(@RequestBody @Valid LoginDTO dto){
         var usernamePassword = new UsernamePasswordAuthenticationToken(dto.email(), dto.senha());
         var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        String token = tokenService.criarToken((Usuario) auth.getPrincipal());
+        return new LoginResponseDTO(token);
     }
 
     @PostMapping("/registrar")
