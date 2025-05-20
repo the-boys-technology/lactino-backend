f749832f2cea2db3d2b13aba3c6ca6b8e37bc4d9
Guiioff <guilhermerivaldy288@hotmail.com>
2025-05-20 18:53:55 -0300

    feat: adiciona usuario no filtro para listar leites

--- src/main/java/br/com/tbt/lactino/controller/request/LeiteFiltro.java
+++ src/main/java/br/com/tbt/lactino/controller/request/LeiteFiltro.java
@@ -1,10 +1,12 @@
 package br.com.tbt.lactino.controller.request;
 
+import br.com.tbt.lactino.model.Usuario;
 import br.com.tbt.lactino.model.enums.StatusLeiteEnum;
 import br.com.tbt.lactino.model.enums.TurnoEnum;
 
 public record LeiteFiltro(
         StatusLeiteEnum status,
         String origem,
-        TurnoEnum turno
+        TurnoEnum turno,
+        Usuario usuario
 ) {}
