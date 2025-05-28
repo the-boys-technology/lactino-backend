7d5ef9c8ad9a3526de6fc7d8309c5ba2c02a3b31
Guiioff <guilhermerivaldy288@hotmail.com>
2025-05-27 22:53:52 -0300

    refactor: alteração da estrutura de transacaoDTO

---
+++ src/main/java/br/com/tbt/lactino/controller/request/ItemTransacaoDTO.java
@@ -0,0 +1,15 @@
+package br.com.tbt.lactino.controller.request;
+
+import br.com.tbt.lactino.model.enums.CategoriaProduto;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record ItemTransacaoDTO(
+        @NotNull UUID produtoId,
+        @NotNull CategoriaProduto categoria,
+        @NotNull Integer quantidade,
+        @NotNull BigDecimal precoUnitario
+) {
+}
