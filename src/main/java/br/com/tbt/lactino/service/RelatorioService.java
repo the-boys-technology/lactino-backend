213239e983bbd644921bf615abfa5cfd74d39d7f
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-05-28 22:21:18 -0300

    feat: declarando método para geração do PDF do relatório

--- src/main/java/br/com/tbt/lactino/service/RelatorioService.java
+++ src/main/java/br/com/tbt/lactino/service/RelatorioService.java
@@ -3,5 +3,7 @@ package br.com.tbt.lactino.service;
 import br.com.tbt.lactino.controller.response.RelatorioPedidoResponse;
 
 public interface RelatorioService {
-    RelatorioPedidoResponse gerarRelatorioPedido(Long transacaoId);
+  RelatorioPedidoResponse gerarRelatorioPedido(Long transacaoId);
+
+  byte[] gerarRelatorioPedidoPdf(Long transacaoId);
 }
