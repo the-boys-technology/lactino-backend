75c1ad9e0dd42748b17533cd55bc51d934ba7672
Guiioff <guilhermerivaldy288@hotmail.com>
2025-06-03 21:55:11 -0300

    feat: define e implementa service responsável por busca do endereço na api viacep

---
+++ src/main/java/br/com/tbt/lactino/service/impl/EnderecoServiceImpl.java
@@ -0,0 +1,27 @@
+package br.com.tbt.lactino.service.impl;
+
+import br.com.tbt.lactino.service.EnderecoService;
+import br.com.tbt.lactino.service.dto.ViaCepResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+public class EnderecoServiceImpl implements EnderecoService {
+
+    private final WebClient webClient = WebClient.create("https://viacep.com.br/ws");
+
+    @Override
+    public ViaCepResponse buscarEnderecorPorCep(String cep) {
+        try {
+            return webClient
+                    .get()
+                    .uri("/{cep}/json/", cep)
+                    .retrieve()
+                    .bodyToMono(ViaCepResponse.class)
+                    .block();
+        } catch (WebClientResponseException.NotFound e) {
+            throw new RuntimeException("CEP não encontrado.");
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao consultar o CEP: " + e.getMessage());
+        }
+    }
+}
