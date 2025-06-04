699df2ec0bace937511a1458437a55fd2598f0e2
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-06-04 20:55:26 -0300

    feat: criando classe de serviço para envio de email

---
+++ src/main/java/br/com/tbt/lactino/service/impl/EnviarEmailServiceImpl.java
@@ -0,0 +1,11 @@
+package br.com.tbt.lactino.service.impl;
+
+import br.com.tbt.lactino.service.EnviarEmailService;
+import br.com.tbt.lactino.service.dto.EnviarEmailDTO;
+
+public class EnviarEmailServiceImpl implements EnviarEmailService {
+  @Override
+  public void enviarEmail(EnviarEmailDTO dto) {
+    // Implementar o envio de emails aqui
+  }
+}
