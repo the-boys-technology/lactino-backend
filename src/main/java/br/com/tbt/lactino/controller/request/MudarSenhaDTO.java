162f257ec9be698d44b4d22a291d3d02d06ba4e2
Gabriel Ferreira <gabrielfsilva137@gmail.com>
2025-06-04 21:32:13 -0300

    feat: implementando validação de senha do usuário

--- src/main/java/br/com/tbt/lactino/controller/request/MudarSenhaDTO.java
+++ src/main/java/br/com/tbt/lactino/controller/request/MudarSenhaDTO.java
@@ -1,3 +1,12 @@
 package br.com.tbt.lactino.controller.request;
 
-public record MudarSenhaDTO(String senhaAtual, String novaSenha) {}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record MudarSenhaDTO(
+    @NotBlank(message = "É obrigatório informar a senha atual") String senhaAtual,
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[!@#$%^&*()_+=\\[\\]{};':\"\\\\|,.<>/?-]).{8,}$",
+            message =
+                "A senha precisa ter, no mínimo, 8 caracteres, com pelo menos um número e um caractere especial")
+        String novaSenha) {}
--- src/main/java/br/com/tbt/lactino/controller/request/ResetarSenhaDTO.java
+++ src/main/java/br/com/tbt/lactino/controller/request/ResetarSenhaDTO.java
@@ -2,10 +2,15 @@ package br.com.tbt.lactino.controller.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record ResetarSenhaDTO(
     @NotBlank(message = "É obrigatório informar o email")
         @Email(message = "O email informado deve ser válido")
         String email,
     @NotBlank(message = "É obrigatório informar o código enviado para o usuário") String codigo,
-    String novaSenha) {}
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[!@#$%^&*()_+=\\[\\]{};':\"\\\\|,.<>/?-]).{8,}$",
+            message =
+                "A senha precisa ter, no mínimo, 8 caracteres, com pelo menos um número e um caractere especial")
+        String novaSenha) {}
