9f3da3d3fdbc32f705f709d875768370350b1c20
Guiioff <guilhermerivaldy288@hotmail.com>
2025-06-24 21:26:45 -0300

    refactor: altera controller de health

--- src/main/java/br/com/tbt/lactino/controller/HealthController.java
+++ src/main/java/br/com/tbt/lactino/controller/HealthController.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/health")
 public class HealthController {
 
     @GetMapping
-    public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("OK");
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "OK");
+        return ResponseEntity.ok(response);
     }
 }
