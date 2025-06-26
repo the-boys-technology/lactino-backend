08e1cb56aadd17ece9fd8239debd3c30569a4684
Guiioff <guilhermerivaldy288@hotmail.com>
2025-06-25 21:29:00 -0300

    fix: corrige processo de envio de email

--- src/main/java/br/com/tbt/lactino/service/impl/EnviarEmailServiceImpl.java
+++ src/main/java/br/com/tbt/lactino/service/impl/EnviarEmailServiceImpl.java
@@ -2,11 +2,10 @@ package br.com.tbt.lactino.service.impl;
 
 import br.com.tbt.lactino.service.EnviarEmailService;
 import br.com.tbt.lactino.service.dto.EnviarEmailDTO;
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,19 +13,18 @@ import org.springframework.stereotype.Service;
 public class EnviarEmailServiceImpl implements EnviarEmailService {
   private final JavaMailSender mailSender;
 
-  @Value("${spring.mail.username}")
+  @Value("${emailFrom}")
   private String emailFrom;
 
   @Override
   public void enviarEmail(EnviarEmailDTO dto) {
     try {
-      MimeMessage message = this.mailSender.createMimeMessage();
-      MimeMessageHelper helper = new MimeMessageHelper(message, true);
+      SimpleMailMessage message = new SimpleMailMessage();
 
-      helper.setFrom(emailFrom);
-      helper.setTo(dto.to());
-      helper.setSubject(dto.subject());
-      helper.setText(dto.body());
+      message.setFrom(emailFrom);
+      message.setTo(dto.to());
+      message.setSubject(dto.subject());
+      message.setText(dto.body());
 
       this.mailSender.send(message);
     } catch (Exception ex) {
